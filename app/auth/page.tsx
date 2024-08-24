c19bda1b6682f4a51a3b5520de43669afa7f7188
Diego Gallovich <diego@vitaliti.world>
2024-08-24 16:39:46 +0200

    Basic auth redirect page to greet users

---
+++ app/auth/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AuthPage = () => {
+  return (
+    <div>
+      <h1>You've successfully authenticated your Webflow site</h1>
+    </div>
+  );
+};
+
+export default AuthPage;
