3127336df532645657c97a3063af2182a662a1e5
Diego Gallovich <diego@vitaliti.world>
2024-08-24 16:42:53 +0200

    BUGFIX: Unscaped entities

--- app/auth/page.tsx
+++ app/auth/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AuthPage = () => {
   return (
     <div>
-      <h1>You've successfully authenticated your Webflow site</h1>
+      <h1>You&apos;ve successfully authenticated your Webflow site</h1>
     </div>
   );
 };
