4db4ec809809ddc94361c99c44f009d199611c92
Diego Gallovich <diego@vitaliti.world>
2024-09-09 23:22:19 +0200

    Lint and abstraction of getEnvVar util

--- app/logto.ts
+++ app/logto.ts
@@ -1,8 +1,10 @@
+import { getEnvVariable } from '@/utils/get-env-variable';
+
 export const logtoConfig = {
-  endpoint: process.env.LOGTO_ENDPOINT!,
-  appId: process.env.LOGTO_APP_ID!,
-  appSecret: process.env.LOGTO_APP_SECRET!,
-  baseUrl: process.env.NEXT_PUBLIC_SITE_URL!,
-  cookieSecret: process.env.LOGTO_COOKIE_SECRET!,
+  endpoint: getEnvVariable('LOGTO_ENDPOINT'),
+  appId: getEnvVariable('LOGTO_APP_ID'),
+  appSecret: getEnvVariable('LOGTO_APP_SECRET'),
+  baseUrl: getEnvVariable('NEXT_PUBLIC_SITE_URL'),
+  cookieSecret: getEnvVariable('LOGTO_COOKIE_SECRET'),
   cookieSecure: process.env.NODE_ENV === 'production',
 };
