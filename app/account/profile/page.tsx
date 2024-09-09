c925583beafe5cf57e3b98875281db740b780d97
Diego Gallovich <diego@vitaliti.world>
2024-09-09 23:28:07 +0200

    modified profile page to use logto instead of supabase auth

--- app/account/profile/page.tsx
+++ app/account/profile/page.tsx
@@ -1,21 +1,21 @@
-import { createServerComponentClient } from '@supabase/auth-helpers-nextjs';
-import { cookies } from 'next/headers';
+import { getLogtoContext } from '@logto/next/server-actions';
+import { logtoConfig } from '@/app/logto';
 import { Text } from '@/components/ui/text';
 import { redirect } from 'next/navigation';
 
 export default async function Profile() {
-  const supabase = createServerComponentClient({ cookies });
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const { isAuthenticated, claims } = await getLogtoContext(logtoConfig);
 
-  if (!user) {
-    redirect('/login');
+  if (!isAuthenticated) {
+    // Redirect to home page if user is not authenticated
+    return redirect('/');
   }
+
+  const userEmail = claims?.email;
 
   return (
     <div className="mx-auto mt-10 max-w-2xl">
-      <Text className="mb-6 text-3xl font-bold">Welcome to your account profile, {user.email}</Text>
+      <Text className="mb-6 text-3xl font-bold">Welcome to your account profile, {userEmail}</Text>
     </div>
   );
 }
