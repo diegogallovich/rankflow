252a63b2522db687d398feeda0c4f221d16c73f7
Diego Gallovich <diego@vitaliti.world>
2024-09-15 13:50:53 +0200

    Logto constants for api resources and scopes

--- utils/constants.ts
+++ utils/constants.ts
@@ -6,3 +6,10 @@ export const ALLOWED_ORIGINS = [
   getEnvVariable('LOGTO_ENDPOINT'),
   getEnvVariable('NEXT_PUBLIC_SITE_URL'),
 ];
+
+export const LOGTO_API_RESOURCES = [
+  `${getEnvVariable('NEXT_PUBLIC_SITE_URL')}/sites`,
+  `${getEnvVariable('NEXT_PUBLIC_SITE_URL')}/collections`,
+];
+
+export const LOGTO_API_SCOPES = ['read', 'write', 'delete'];
